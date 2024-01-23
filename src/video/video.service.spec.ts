1d2cec218f566991d02c42b3391d76b226317757
Matheus de Gondra <matheusgondra951@gmail.com>
2024-01-23 09:16:35 -0300

    test: ensure VideoService create a video

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -1,20 +1,43 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { VideoService } from "./video.service";
-import { PrismaService } from "../prisma/prisma.service";
+import { VideoEntity } from "./entities/video.entity";
 import { VideoRepository } from "./repositories/video.repository";
+import { VideoService } from "./video.service";
+import { CreateVideoDTO } from "./dtos/create-video.dto";
 
 describe("VideoService", () => {
 	let service: VideoService;
+	let videoEntityMock: VideoEntity;
 
 	beforeEach(async () => {
+		const repositoryMock = {
+			create: jest.fn().mockResolvedValue(Promise.resolve(videoEntityMock)),
+			findAll: jest.fn().mockResolvedValue(Promise.resolve([videoEntityMock])),
+			findById: jest.fn().mockResolvedValue(Promise.resolve(videoEntityMock)),
+			update: jest.fn().mockResolvedValue(Promise.resolve(videoEntityMock)),
+			delete: jest.fn().mockResolvedValue(Promise.resolve(videoEntityMock))
+		};
+
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [VideoRepository, PrismaService, VideoService]
+			providers: [
+				{ provide: VideoRepository, useValue: repositoryMock },
+				VideoService
+			]
 		}).compile();
 
 		service = module.get<VideoService>(VideoService);
 	});
 
-	it("should be defined", () => {
+	it("Should be defined", () => {
 		expect(service).toBeDefined();
+	});
+
+	it("Should create a video", async () => {
+		const createVideoDTO: CreateVideoDTO = {
+			title: "any_title",
+			description: "any_description",
+			url: "any_url"
+		};
+		const video = await service.create(createVideoDTO);
+		expect(video).toEqual(videoEntityMock);
 	});
 });
